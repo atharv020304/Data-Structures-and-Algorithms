25a188f07ed2a04933cc165f5dce1b2e6219fb75
Atharv Bapat <atharvabapat3@gmail.com>
2025-11-16 20:20:24 +0530

    segtree build method added

---
+++ SegmentTree/SegmentTree.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SegmentTree
+{
+public:
+    int n; // size
+    vector<long long> tree;
+
+    SegmentTree(int n)
+    {
+        this->n = n;
+        tree.resize(4 * n); // safe upper bound in worst case recursion shape
+    }
+
+    void BuildTree(vector<int> &arr, int index, int left, int right)
+    {
+        if (left == right)
+        {
+            tree[index] = arr[left];
+            return;
+        }
+        int mid = (left + right) / 2;
+        BuildTree(arr, 2 * index, left, mid);
+        BuildTree(arr, (2 * index) + 1, mid + 1, right);
+        tree[index] = tree[2 * index] + tree[(2 * index) + 1];
+    }
+};
+
+int main()
+{
+
+    vector<int> arr = {5, 1, 2, 6, 7, 8, 4, 3};
+    SegmentTree st(arr.size());
+    st.BuildTree(arr, 1, 0, arr.size() - 1);
+    return 0;
+}
