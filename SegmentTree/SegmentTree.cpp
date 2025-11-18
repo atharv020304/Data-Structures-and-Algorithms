0920c4ff0fdf353894ae320540c90c84dcdd4eed
Atharv Bapat <atharvabapat3@gmail.com>
2025-11-18 08:32:17 +0530

    update query

--- SegmentTree/SegmentTree.cpp
+++ SegmentTree/SegmentTree.cpp
@@ -25,6 +25,21 @@ public:
         BuildTree(arr, (2 * index) + 1, mid + 1, right);
         tree[index] = tree[2 * index] + tree[(2 * index) + 1];
     }
+
+    void update(int idx, int val, int i, int l, int r)
+    {
+        if (l == r)
+        {
+            tree[i] = val;
+            return;
+        }
+        int mid = (l + r) / 2;
+        if (idx <= mid)
+            update(idx, val, 2 * i, l, mid);
+        else
+            update(idx, val, 2 * i + 1, mid + 1, r);
+        tree[i] = tree[2 * i] + tree[2 * i + 1];
+    }
 };
 
 int main()
@@ -33,5 +48,6 @@ int main()
     vector<int> arr = {5, 1, 2, 6, 7, 8, 4, 3};
     SegmentTree st(arr.size());
     st.BuildTree(arr, 1, 0, arr.size() - 1);
+    st.update(3, 10, 1, 0, arr.size() - 1);
     return 0;
 }
