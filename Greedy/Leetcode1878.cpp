bfd7d5f73e605431e2872e1fa4524ca4df5ab2a4
Atharv Bapat <atharvabapat3@gmail.com>
2026-03-22 22:59:48 +0530

    18th March

---
+++ Greedy/Leetcode1878.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        set<int> sums;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                sums.insert(grid[i][j]);
+
+                for(int k = 1; ; k++){
+                    
+                    int r = i + 2*k;
+                    int left = j - k;
+                    int right = j + k;
+
+                    if(r >= m || left < 0 || right >= n) break;
+
+                    int sum = 0;
+
+                    int x = i, y = j;
+
+                    for(int t = 0; t < k; t++){
+                        sum += grid[x+t][y+t];
+                    }
+
+                    for(int t = 0; t < k; t++){
+                        sum += grid[x+k+t][y+k-t];
+                    }
+
+                    for(int t = 0; t < k; t++){
+                        sum += grid[x+2*k-t][y-t];
+                    }
+
+                    for(int t = 0; t < k; t++){
+                        sum += grid[x+k-t][y-k+t];
+                    }
+
+                    sums.insert(sum);
+                }
+            }
+        }
+
+        vector<int> ans;
+        for(auto it = sums.rbegin(); it != sums.rend() && ans.size() < 3; ++it){
+            ans.push_back(*it);
+        }
+        return ans;
+    }
+};
