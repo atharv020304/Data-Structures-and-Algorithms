63d987afb0cc3e56c536a7d11499a032c7fd0f85
Atharv Bapat <atharvabapat3@gmail.com>
2026-04-19 18:15:01 +0530

    brute force added 19th Apr

--- BinarySearch/Leetcode-1855.cpp
+++ BinarySearch/Leetcode-1855.cpp
@@ -1,3 +1,23 @@
+//TLE
+// class Solution {
+// public:
+//     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+//         int maxD = INT_MIN;
+//         for(int i = 0 ; i < nums1.size(); i++)
+//         {
+//             for(int j = 0 ; j < nums2.size(); j++)
+//             {
+//                 if(i <= j && nums1[i] <= nums2[j])
+//                 {
+//                     maxD = max( maxD , j - i );
+//                 }
+//             }
+//         }
+//         return maxD > 0 ? maxD : 0 ;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
