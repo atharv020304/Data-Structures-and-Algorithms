6b1bafe6290b0a03671cc5373fa1b7656203ec09
Atharv Bapat <atharvabapat3@gmail.com>
2026-04-19 18:14:24 +0530

    bs

---
+++ BinarySearch/Leetcode-1855.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+
+        for (int i = 0; i < nums1.size(); i++) {
+
+            int low = i, high = nums2.size() - 1;
+            int best = -1;
+
+            while (low <= high) {
+                int mid = (low + high) / 2;
+
+                if (nums2[mid] >= nums1[i]) {
+                    best = mid;       // valid
+                    low = mid + 1;    // try to go further right
+                } else {
+                    high = mid - 1;   // go left
+                }
+            }
+
+            if (best != -1) {
+                ans = max(ans, best - i);
+            }
+        }
+
+        return ans;
+    }
+};
