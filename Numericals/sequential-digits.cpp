5caf8beedc313a82e6596048f05b934089122343
Atharv Bapat <134592383+atharv020304@users.noreply.github.com>
2026-07-13 21:39:34 +0530

    sequential-digits.cpp approch 2

--- Numericals/sequential-digits.cpp
+++ Numericals/sequential-digits.cpp
@@ -15,3 +15,23 @@
 //         return ans;
 //     }
 // };
+
+
+//simpler approch
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string digits = "123456789";
+        vector<int> ans;
+        
+        for (int len = 1; len <= 9; ++len) {
+            for (int i = 0; i + len <= 9; ++i) {
+                int num = stoi(digits.substr(i, len));
+                if (num >= low && num <= high) {
+                    ans.push_back(num);
+                }
+            }
+        }
+        return ans;
+    }
+};
