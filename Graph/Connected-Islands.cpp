7e08115fc5dbe0e4ac581a61f26d1c4fde19f319
Atharv Bapat <134592383+atharv020304@users.noreply.github.com>
2026-08-31 19:50:57 +0530

    stack

--- Graph/Connected-Islands.cpp
+++ Graph/Connected-Islands.cpp
@@ -66,3 +66,61 @@ public:
         return ans;
     }
 };
+
+
+
+//using dfs stack 
+
+class Solution {
+public: 
+
+    int ans = 0;
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j)
+    {
+        vector<int> dirX = { 0,0,1,-1};
+        vector<int> dirY = {1,-1,0,0 };
+
+        int currX = i;
+        int currY = j;
+
+        stack<pair<int,int>> st;
+        visited[currX][currY] = true;
+        st.push({currX,currY});
+
+        while(!st.empty())
+        {
+            auto temp = st.top();
+            st.pop();
+
+            for(int i = 0 ; i < 4 ; i++)
+            {
+                int newX = temp.first + dirX[i];
+                int newY = temp.second + dirY[i];
+
+                if(newX >=0 && newY >= 0 && newX < grid.size() && newY < grid[0].size() && grid[newX][newY] == '1' && !visited[newX][newY])
+                {
+                    visited[newX][newY] = true;
+                    st.push({newX,newY});
+                }
+            }
+        }
+        ans ++;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        for(int i = 0 ; i < m; i++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                if(!visited[i][j] && grid[i][j] == '1')
+                {
+                    dfs(grid,visited,i,j);
+                }
+            }
+        }
+        return ans;
+    }
+};
